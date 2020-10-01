a59ebcadb223078eb84c01e21a1ae60ce52d734d
muskan090101 <56040630+muskan090101@users.noreply.github.com>
2020-10-01 11:44:00 +0530

    Added main method

--- Calculator.java
+++ Calculator.java
@@ -4,5 +4,11 @@ package hcl;
 	   public int add(int a, int b){
 	          return a+b;
 	   }
+		public static void main(String args[])
+		{
+			int a = 10;
+			int b = 20;
+			add(a,b);
+		}
 	}
 
