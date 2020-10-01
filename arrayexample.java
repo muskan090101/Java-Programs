b05b1054f7afb1b37f8837f8db42013b76c8d90e
muskan090101 <56040630+muskan090101@users.noreply.github.com>
2020-10-01 11:58:09 +0530

    Added new java files
    One java program for showing the array example
    One java program for showing the string example

---
+++ arrayexample.java
@@ -0,0 +1,52 @@
+package practiceforTI;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class arrayexample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		int b[] = {1,2,3,4,5};
+		int a[] = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+		
+		for(int i =0; i<a.length; i++)
+		{
+			
+			System.out.print(a[i]+" ");
+		
+		}
+		System.out.println("");
+		Arrays.sort(a);
+		for(int i =0; i<a.length; i++)
+		{
+			
+			System.out.print(a[i]+" ");
+		
+		}
+		System.out.println();
+		System.out.println(Arrays.asList(a));
+		System.out.println(Arrays.binarySearch(a, 3));
+		
+		
+		System.out.println();
+		
+		Arrays.toString(a);
+		
+		for(int i =0; i<a.length; i++)
+		{
+			
+			
+			System.out.print(a[i]+" ");
+		
+		}
+		
+		
+		
+		
+
+		}
+
+}
---
+++ stringexample.java
@@ -0,0 +1,24 @@
+package practiceforTI;
+
+public class stringexample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String s = "Hello";
+		System.out.println(s+" World");
+		
+		System.out.println();
+		
+		
+		int a = 10; 
+		double b = 50.20;
+		
+		char name = 'a';
+			
+		System.out.printf("My name is %c and my age is %d and my marks are %f", name, a, b);
+		
+		
+	}
+
+}
